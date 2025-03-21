14a93acfe53851b67deda82d9df6145d80610263
dlaboy <diego.laboy1@upr.edu>
2025-03-21 15:22:42 -0400

    running test on local environment

--- test/e2e/register.test.js
+++ test/e2e/register.test.js
@@ -7,7 +7,7 @@ const { default: createDriver } = require('./driver');
 
 async function registerTest(name,email,password) {
   // Create a new browser instance (Chrome)
-  const driver = await createDriver({headless:true});
+  const driver = await createDriver({headless:false});
 
 
   try {
