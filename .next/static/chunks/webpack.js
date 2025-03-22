7a210871f96f18679340e05b8a33ab462c518f60
dlaboy <diego.laboy1@upr.edu>
2025-03-22 12:41:21 -0400

    automating load test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "96f60e41641bfe47"; }
+/******/ 		__webpack_require__.h = function() { return "2afbf87130a2d731"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1400,11 +1400,6 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	!function() {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- .next/static/webpack/webpack.bfd5ad32c79fdbff.hot-update.js
+++ .next/static/webpack/webpack.bfd5ad32c79fdbff.hot-update.js
@@ -11,12 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "96f60e41641bfe47"; }
-/******/ }();
-/******/ 
-/******/ /* webpack/runtime/nonce */
-/******/ !function() {
-/******/ 	__webpack_require__.nc = undefined;
+/******/ 	__webpack_require__.h = function() { return "2afbf87130a2d731"; }
 /******/ }();
 /******/ 
 /******/ }
